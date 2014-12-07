fa636e5a4e9f64e0882e23f573dd4674caa7fade
QiMeng <616684889@qq.com>
2014-12-07 22:19:42 +0800

    .

---
+++ CTest/CTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CTest
+//
+//  Created by QiMENG on 14-12-7.
+//  Copyright (c) 2014年 QiMENG. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
--- IOTest/IOTest/main.cpp
+++ IOTest/IOTest/main.cpp
@@ -8,15 +8,68 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-   
-    /**
-     *  输入两个数字, 打印出来
-     */
-    std::cout << "Enter two numbers:" << std::endl;
-    int v1,v2;
-    std::cin >>v1 >> v2;
-    std::cout << "The sum of " << v1 << " and " << v2 << std::endl;
+using namespace std;
 
+class Foo {
+    
+    
+    
+};  // 不要忘记有分号
+
+
+
+int main(int argc, const char * argv[]) {
+
+//    int i , &ri = i;
+//    i = 3;
+//    ri = 19;
+//    std::cout << i << ri << std::endl;
+    
+//    /**
+//     *  输入两个数字, 打印出来
+//     */
+//    std::cout << "Enter two numbers:" << std::endl;
+//    int v1,v2;
+//    std::cin >>v1 >> v2;
+//    std::cout << "The sum of " << v1 << " and " << v2 << std::endl;
+    
+//    string word;
+//    while (std::cin >> word) {
+//        
+//        std::cout << word << std::endl;
+//    }
+//    while (getline(cin, word)) {
+//        cout << word << endl;
+//        for (string::size_type i= 0; i< word.size(); ++i) {
+//            cout << word[i] << endl;
+//        }
+//    }
+    
+//    //读入两个string,分别比较字符, 比较长度
+//    string temp1,temp2;
+//    cout << "输入两个字符串" << endl;
+//    cin >> temp1 >> temp2;
+//    if (temp1 == temp2) {
+//        cout << "两个字符串相同" << endl;
+//    }
+//    else if (temp1 > temp2) {
+//        cout << "temp1大" << endl;
+//    }
+//    else {
+//        cout << "temp2大" << endl;
+//    }
+//    if (temp1.size() >= temp2.size()) {
+//        cout << temp1 << ">=" << temp2 << endl;
+//    }else {
+//        cout << temp1 << "<" << temp2 << endl;
+//    }
+    
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
